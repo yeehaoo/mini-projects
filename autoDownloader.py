08859173bf197c82bf688bab2a3020a7bba73428
Yee Hao <23189791+yeehaoo@users.noreply.github.com>
2018-11-09 18:31:35 +0800

    Update autoDownloader.py

--- autoDownloader.py
+++ autoDownloader.py
@@ -1,3 +1,16 @@
+"""
+What is this script?
+This script goes to your clipboard, saves the image from the URL, and names it according to the number of files in the current directory
+(if you have 5 files in the current directory, the image will be saved as '6.jpg' or '6.png', depending on the original file extension.
+
+Instructions:
+1. Make sure you have python3 and pywin32 installed. (run 'pip install pywin32' if you do not)
+2. Download the script and save it in the directory in which you want to save the image(s).
+3. Copy the direct link of an image. Make sure the URL links directly to the image, and not an image viewer
+('i.imgur.com/foobar.jpg' instead of 'imgur.com/gallery/foobar' for example)
+4. Run the script (via commandline or windows explorer)
+"""
+
 import requests
 import win32clipboard
 import shutil
