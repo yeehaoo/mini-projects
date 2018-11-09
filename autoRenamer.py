1f1dec1474f7fdccdee95161b9aa2c25bbef17a8
Yee Hao <23189791+yeehaoo@users.noreply.github.com>
2018-11-09 16:16:10 +0800

    Update autoRenamer.py

--- autoRenamer.py
+++ autoRenamer.py
@@ -7,13 +7,16 @@ parser.add_argument('-p', '--path', help = 'path of directory', required = True)
 args = vars(parser.parse_args())
 
 pathToRename = args['path']
+if(pathToRename[-1] != '/'):
+    pathToRename = pathToRename + '/'
+    
 count = 0
 
 for folderName, subfolders, filenames in os.walk(pathToRename):
     print('The current folder is ' + folderName)
     for filename in filenames:
         count += 1
-        print('FILE INSIDE ' + folderName + ': '+ filename)
-        shutil.move(pathToRename + filename, pathToRename + str(count) + filename[-4:])
-        
+        newFileName = str(count) + filename[-4:]
+        shutil.move(pathToRename + filename, pathToRename + newFileName)
+        print(filename + ' renamed to ' + newFileName)
     print('Renaming complete, ' + str(count) + ' files renamed.')
