a22f15d5de3dcca1f9ebf8d75da9df3ca5ecb668
Yee Hao <23189791+yeehaoo@users.noreply.github.com>
2018-11-09 16:25:46 +0800

    Update autoDownloader.py

--- autoDownloader.py
+++ autoDownloader.py
@@ -1,11 +1,22 @@
 import requests
 import win32clipboard
+import shutil
+import os
 
+#get url from clipboard
 win32clipboard.OpenClipboard()
 url = win32clipboard.GetClipboardData()
 win32clipboard.CloseClipboard()
 
+count = 0
+
+#count number of files in current directory
+for folderName, subfolders, filenames in os.walk('./'):
+    for filename in filenames:
+        count += 1
+
+#download file and save as 1 more than count
 response = requests.get(url)
 if response.status_code == 200:
-    with open("./file.jpg", 'wb') as f:
+    with open('./' + str(count+1) + url[-4:], 'wb') as f:
         f.write(response.content)
